19af0135b19e52e444338a5edc39cfaceaae3b56
Kotaro Takahama <kotaro.takahama@septeni-incubate.co.jp>
2023-03-23 18:56:26 +0900

    nextjsテンプレ

--- src/pages/events/[eventId].tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPageWithLayout } from "next";
-
-import { UserLayout } from "@/components/layouts/user";
-import { UserEventsItemPageComponent } from "@/components/pageComponents/user/events/eventId";
-
-const Page: NextPageWithLayout = () => <UserEventsItemPageComponent />;
-Page.getLayout = (page) => <UserLayout>{page}</UserLayout>;
-export default Page;
--- src/pages/events/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPageWithLayout } from "next";
-
-import { UserLayout } from "@/components/layouts/user";
-import { UserEventsPageComponent } from "@/components/pageComponents/user/events/index";
-
-const Page: NextPageWithLayout = () => <UserEventsPageComponent />;
-Page.getLayout = (page) => <UserLayout>{page}</UserLayout>;
-export default Page;
